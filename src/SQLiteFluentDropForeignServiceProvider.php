88926b2492fff9b9fa4725c8789b90d67422b25a
Alexandre D'Eschambeault <adeschambeault@exolnet.com>
2019-12-19 09:49:26 -0500

    Remove useless boot method in the service provider

--- src/SQLiteFluentDropForeignServiceProvider.php
+++ src/SQLiteFluentDropForeignServiceProvider.php
@@ -13,14 +13,6 @@ use Illuminate\Support\ServiceProvider;
 class SQLiteFluentDropForeignServiceProvider extends ServiceProvider
 {
     /**
-     * Bootstrap the application services.
-     */
-    public function boot()
-    {
-        //
-    }
-
-    /**
      * Register the application services.
      */
     public function register()
